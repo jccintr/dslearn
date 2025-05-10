0ed641a6745afac3f6e76ca6707219bde8f1edf4
Julio Cesar <jccintr@gmail.com>
2025-05-10 15:26:14 -0300

    incluido entidades do forum

--- src/main/java/com/jcsoftware/dslearn/entities/Reply.java
+++ src/main/java/com/jcsoftware/dslearn/entities/Reply.java
@@ -1,60 +1,63 @@
 package com.jcsoftware.dslearn.entities;
 
-
-
-import java.util.ArrayList;
+import java.time.Instant;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = "replies")
+public class Reply {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
-	@Column(unique = true)
-	private String email;
-	private String password;
+
+	@Column(columnDefinition="TEXT")	
+	private String body;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
 	
-	@ManyToMany(fetch=FetchType.EAGER)
-	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-	private Set<Role> roles = new HashSet<>();
+	@ManyToOne
+	@JoinColumn(name = "topic_id")
+	private Topic topic;
 	
-	@OneToMany(mappedBy = "user")
-	private List<Notification> notifications = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	private User author;
 	
-	@OneToMany(mappedBy = "author")
-	private List<Topic> topics = new ArrayList<>();
-	
+	@ManyToMany
+	@JoinTable(name = "reply_likes",
+		joinColumns = @JoinColumn(name = "reply_id"),
+		inverseJoinColumns = @JoinColumn(name = "user_id")
+	)
+	private Set<User> likes = new HashSet<>();
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -63,9 +66,10 @@ public class User {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Reply other = (Reply) obj;
 		return Objects.equals(id, other.id);
 	}
 
+	
 	
 }
--- src/main/java/com/jcsoftware/dslearn/entities/Topic.java
+++ src/main/java/com/jcsoftware/dslearn/entities/Topic.java
@@ -1,76 +1,77 @@
 package com.jcsoftware.dslearn.entities;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name = "lessons")
-@Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Lesson {
+@Table(name = "topics")
+public class Topic {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
-	private Integer position;
+
+	@Column(columnDefinition = "TEXT")
+	private String body;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
 	
 	@ManyToOne
-	@JoinColumn(name = "section_id")
-	private Section section;
+	@JoinColumn(name = "lesson_id")
+	private Lesson lesson;
+	
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	private User author;
+	
+	@ManyToOne
+	@JoinColumn(name="offer_id")
+	private Offer offer;
+	
+	@ManyToOne
+	@JoinColumn(name = "reply_id")
+	private Reply answer;
 	
 	@ManyToMany
-	@JoinTable(name = "lessons_done",
-		joinColumns = @JoinColumn(name = "lesson_id"),
-		inverseJoinColumns = {
-				@JoinColumn(name = "user_id"),
-				@JoinColumn(name = "offer_id")
-		}
-	)
-	private Set<Enrollment> enrollmentsDone = new HashSet<>();
+	@JoinTable(name = "topic_likes",
+		joinColumns = @JoinColumn(name = "topic_id"),
+		inverseJoinColumns = @JoinColumn(name = "user_id"))	
+	private Set<User> likes = new HashSet<>();
 	
-	@OneToMany(mappedBy = "lesson")
-	private List<Deliver> deliveries = new ArrayList<>();
-	
-	@OneToMany(mappedBy = "lesson")
-	private Set<Topic> topics = new HashSet<>();
-	
-	
-	
-	
-	public Lesson(Long id, String title, Integer position, Section section) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.position = position;
-		this.section = section;
-	}
+	@OneToMany(mappedBy = "topic")
+	private List<Reply> replies = new ArrayList<>();
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -79,10 +80,9 @@ public abstract class Lesson {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Lesson other = (Lesson) obj;
+		Topic other = (Topic) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
+	
 }
